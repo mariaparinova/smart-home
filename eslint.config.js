68f39eaccfe0f1961f8c36b856632f35dee7309e
Maria Parinova <maryia.parynava@gmail.com>
2025-11-30 22:43:03 +0100

    chore: setup Angular project

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+      eslintPluginUnicorn.configs.recommended,
+      prettierConfig,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/catch-error-name': 'off',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
