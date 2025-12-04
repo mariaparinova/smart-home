af3872c6562608a45178c6db6251f0dab496ed44
Maria Parinova <maryia.parynava@gmail.com>
2025-12-04 03:18:11 +0100

    feat: complete sidebar

--- eslint.config.js
+++ eslint.config.js
@@ -42,6 +42,8 @@ export default defineConfig([
   {
     files: ['**/*.html'],
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/prefer-self-closing-tags": ["error"]
+    },
   },
 ]);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './sidebar/sidebar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Sidebar, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  host: {
+    '[class.sidebar-closed]': '!isSidebarOpen()',
+  },
 })
-export class App {}
+export class App {
+  isSidebarOpen = signal(true);
+
+  toggleSidebar = () => {
+    this.isSidebarOpen.update((current) => !current);
+  };
+}
